5a5728ab3cfdd4d0183827ab9bb203277d156dc9
c.winger <c.winger@oeko.de>
2023-02-20 20:08:54 +0100

    complex test

--- data_disaggregation/utils.py
+++ data_disaggregation/utils.py
@@ -1,3 +1,38 @@
+import pandas as pd
+
+
+def as_items(x):
+    if isinstance(x, (dict, pd.Series)):
+        return x.items()
+    return x
+
+
+def as_index(x):
+    if isinstance(x, pd.Series):
+        return x.index
+    return x
+
+
+def as_multi_index(x):
+    if isinstance(x, pd.Series):
+        return pd.Series(x.values, index=as_multi_index(x.index))
+    elif isinstance(x, pd.MultiIndex):
+        return x
+    elif isinstance(x, pd.Index):
+        return pd.MultiIndex.from_product([x])
+    return x
+
+
+def as_single_index(x):
+    if isinstance(x, pd.Series):
+        return pd.Series(x.values, index=as_single_index(x.index))
+    elif isinstance(x, pd.MultiIndex):
+        if len(x.names) != 1:
+            raise Exception("number of levels must be 1")
+        return pd.Index([i[0] for i in x], name=x.names[0])
+    return x
+
+
 def group_sum(key_vals, get_key=None):
     """simple group sum
 
@@ -9,6 +44,8 @@ def group_sum(key_vals, get_key=None):
     Returns
         list of (unique key, sum of values) pairs
     """
+
+    key_vals = as_items(key_vals)
 
     res = {}
     if not get_key:
@@ -93,3 +130,10 @@ def weighted_median(value_normweights):
 
 def is_na(x):
     return x is None
+
+
+def get_levels_dict(idx):
+    if isinstance(idx, pd.MultiIndex):
+        return dict(zip(idx.names, idx.levels))
+    else:
+        return dict({idx.name: idx})
--- test/test.py
+++ test/test.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 from data_disaggregation import minimal_example, vartpye
 from data_disaggregation.utils import (
+    as_multi_index,
+    as_single_index,
     group_sum,
     weighted_median,
     weighted_mode,
@@ -14,7 +16,7 @@ from data_disaggregation.utils import (
 logging.basicConfig(
     format="[%(asctime)s %(levelname)7s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
-    level=logging.INFO,
+    level=logging.DEBUG,
 )
 
 
@@ -40,61 +42,134 @@ class TestUtils(TestCase):
         res = weighted_sum([(3, 0.4), (2, 0.25), [1, 0.35]])
         self.assertAlmostEqual(res, 2.05)
 
+    def test_as_multi_index(self):
+        res = as_multi_index(pd.Index(["a"]))
+        self.assertEqual(res[0], ("a",))
+
+        res = as_multi_index(pd.MultiIndex.from_product([["a"]]))
+        self.assertEqual(res[0], ("a",))
+
+        res = as_multi_index(pd.Series({"a": 1}))
+        self.assertEqual(res.index[0], ("a",))
+
+        res = as_multi_index(pd.Series({("a",): 1}))
+        self.assertEqual(res.index[0], ("a",))
+
+    def test_as_single_index(self):
+        res = as_single_index(pd.Index(["a"]))
+        self.assertEqual(res[0], "a")
+
+        res = as_single_index(pd.MultiIndex.from_product([["a"]]))
+        self.assertEqual(res[0], "a")
+
+        res = as_single_index(pd.Series({"a": 1}))
+        self.assertEqual(res.index[0], "a")
+
+        res = as_single_index(pd.Series({("a",): 1}))
+        self.assertEqual(res.index[0], "a")
+
 
 class TestExample(TestCase):
     """"""
 
-    def get_example(self, var_type):
-        return minimal_example(
-            dom1_dom2_weights={
-                ("a", "F"): 1,
+    def get_example(self, vtype):
+        """
+        M | D  E  F | S | V
+        ====================
+        a |       2 | 2 |  5
+        b | 1     2 | 3 | 10
+        c |    2  1 | 3 | 30
+        ====================
+        S | 1  2  5 | 8 |
+
+        Groups: (value, normweight, rescale)
+        D: [(b, 10, 1/1, 1/3)]
+        E: [(c, 30, 2/2, 2/3)]
+        F: [
+            (a,  5, 2/5, 5/2),
+            (b, 10, 2/5, 5/3),
+            (c, 30, 1/5, 5/3)
+        ]
+
+        """
+        s_map = pd.Series(
+            {
+                ("a", "F"): 2,
                 ("b", "D"): 1,
-                ("b", "F"): 1,
+                ("b", "F"): 2,
                 ("c", "E"): 2,
                 ("c", "F"): 1,
-            },
-            variable={"a": 5, "b": 10, "c": 30},
-            var_type=var_type,
+            }
+        )
+        s_map.index.names = ["d1", "d2"]
+
+        s_var = pd.Series({"a": 5, "b": 10, "c": 30})
+        s_var.index.names = ["d1"]
+
+        return minimal_example(
+            s_map=s_map,
+            s_var=s_var,
+            vtype=vtype,
             as_int=True,
         )
 
-    def test_example(self):
+    def test_example_type_categorical(self):
         res = self.get_example(vartpye.VarTypeCategorical)
         for k, v in {"D": 10, "E": 30, "F": 5}.items():
             self.assertEqual(v, res[k])
 
+    def test_example_type_ordinal(self):
         res = self.get_example(vartpye.VarTypeOrdinal)
         for k, v in {"D": 10, "E": 30, "F": 10}.items():
             self.assertEqual(v, res[k])
 
+    def test_example_type_metric(self):
         res = self.get_example(vartpye.VarTypeMetric)
-        for k, v in {"D": 10, "E": 30, "F": 15}.items():
+        for k, v in {"D": 10, "E": 30, "F": 12}.items():
             self.assertEqual(v, res[k])
 
+    def test_example_type_metric_ext(self):
         res = self.get_example(vartpye.VarTypeMetricExt)
-        for k, v in {"D": 5, "E": 20, "F": 20}.items():
+        for k, v in {"D": round(3.333), "E": 20, "F": round(21.667)}.items():
             self.assertEqual(v, res[k])
 
-    def test_pandas_series(self):
-        """indices are multidimensional (dummy second dim)"""
-        ds_dom1_dom2_weights = pd.Series(
+    def test_x(self):
+        d_A = pd.Index(["a1", "a2", "a3"], name="A")
+        d_B = pd.Index(["b1", "b2"], name="B")
+        d_C = pd.Index(["c1", "c2"], name="C")
+        d_AB = pd.MultiIndex.from_product([d_A, d_B])
+        # d_AC = pd.MultiIndex.from_product([d_A, d_C])
+        d_BC = pd.MultiIndex.from_product([d_B, d_C])
+        # d_ABC = pd.MultiIndex.from_product([d_A, d_B, d_C])
+
+        d_BC_ = pd.MultiIndex.from_tuples(
+            [x for x in d_BC if x != ("b1", "c2")], names=d_BC.names
+        )
+
+        v_AB = pd.Series(
             {
-                (("a", 1), ("F",)): 1,
-                (("b", 1), ("D",)): 1,
-                (("b", 1), ("F",)): 1,
-                (("c", 1), ("E",)): 2,
-                (("c", 1), ("F",)): 1,
-            }
-        )
-        ds_variable = pd.Series({("a", 1): "5", ("b", 1): "10", ("c", 1): "30"})
-
-        res = pd.Series(
-            minimal_example(
-                dom1_dom2_weights=ds_dom1_dom2_weights,
-                variable=ds_variable,
-                var_type=vartpye.VarTypeCategorical,
-            )
+                ("a1", "b1"): 1,
+                ("a1", "b2"): 2,
+                ("a2", "b1"): 3,
+                ("a2", "b2"): 4,
+                ("a3", "b1"): 5,
+                ("a3", "b2"): 6,
+            },
+            index=d_AB,
         )
 
-        for k, v in {("D",): "10", ("E",): "30", ("F",): "5"}.items():
-            self.assertEqual(v, res[k])
+        m_BC = pd.Series(  # noqa
+            {
+                ("b1", "c1"): 1,
+                ("b1", "c2"): 2,
+                ("b2", "c1"): 3,
+                ("b2", "c2"): 4,
+            },
+            index=d_BC,
+        )
+
+        minimal_example(
+            s_var=v_AB,
+            s_map=d_BC_,
+            vtype=vartpye.VarTypeCategorical,
+        )
